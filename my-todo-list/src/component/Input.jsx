3b506dbce349e0c44384e08fc4c99eb3c462d615
gyurison7 <orangetree401@gmail.com>
2023-06-28 18:27:43 +0900

    Input 컴포넌트 form 수정

--- my-todo-list/src/component/Input.jsx
+++ my-todo-list/src/component/Input.jsx
@@ -1,14 +1,14 @@
 const Input = ({ title, body, contentChangeHandler, addButtonHandler }) => {
     return (
-        <div className='add-form'>
+        <form className='add-form'>
             <div className='input-group'>
                 <label className='form-label'>제목</label>
                 <input value={title} onChange={contentChangeHandler} className='add-input' name='title' />
                 <label className='form-label'>내용</label>
                 <input value={body} onChange={contentChangeHandler} className='add-input' name='body' />
-                <button className='add-button' onClick={addButtonHandler}>추가하기</button>
+                <button type="button" className='add-button' onClick={addButtonHandler}>추가하기</button>
             </div>
-        </div>
+        </form>
     )
 }
 
